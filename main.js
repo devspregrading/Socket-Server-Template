0e2e7a917d317c850129c0a37f68a16e9a9fc2d3
SHOVEL <138528960+devspregrading@users.noreply.github.com>
2025-03-26 16:04:28 -0500

    Update main.js

--- main.js
+++ main.js
@@ -10,9 +10,9 @@ const PORT = process.env.PORT || 5001; // Use Heroku's assigned port or 5001 loc
 wss.on("connection", (ws) => {
     console.log("Client connected");
 
-    ws.on("message", (message, roomCode, playerIGN) => {
-        console.log("Received: " + message + roomCode + playerIGN);
-        ws.send("Echo: " + message + roomCode + playerIGN); // Echo message back to client
+    ws.on("message", (message) => {
+        console.log("Received: " + message);
+        ws.send(message); // Echo message back to client
     });
 
     ws.on("close", () => {
